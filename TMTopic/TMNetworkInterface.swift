6363ce5e1610265841e30d616ece63f633adff51
wh1pch81n <wh1pch81n@gmail.com>
2014-11-23 15:51:32 -0800

    Working on second level

--- TMTopic/TMNetworkInterface.swift
+++ TMTopic/TMNetworkInterface.swift
@@ -9,10 +9,10 @@
 import Foundation
 
 enum TMNetwork : Int {
-    case processError = -2
-    case fetchError = -1
-    case fetchUnknown = 0
-    case fetchSuccessful = 1
+  case processError = -2
+  case fetchError = -1
+  case fetchUnknown = 0
+  case fetchSuccessful = 1
 }
 
 private let kFirstLevelXMLBatch = "";
@@ -20,64 +20,109 @@ private let kFirstLevelXMLBatch = "";
 public let TMNetworkInterfaceDidFetchNotification = "TMNetworkInterfaceDidFetchNotification"
 
 class TMNetworkInterface : NSObject, NSXMLParserDelegate {
-    var currentSession : NSURLSession = NSURLSession.sharedSession()
-    var currentNotificationCenter : NSNotificationCenter = NSNotificationCenter.defaultCenter()
-    var firstLevelXMLData : NSData?
-    var firstLevelXMLDict, tempFirstLevelXMLDict : Dictionary<String, NSURL>?
-    var secondLevelXMLDatas : Dictionary<NSURL, NSData>?
-    
-    class var sharedInstance : TMNetworkInterface {
-        struct Static {
-            static let _instance : TMNetworkInterface = TMNetworkInterface()
-        }
-        return Static._instance
+  var delegate : TMNetworkInterfaceDelegate?
+  var currentSession : NSURLSession = NSURLSession.sharedSession()
+  var currentNotificationCenter : NSNotificationCenter = NSNotificationCenter.defaultCenter()
+  //var firstLevelXMLData : NSData?
+  var firstLevelXMLDict, tempFirstLevelXMLDict : Dictionary<String, NSURL>?
+  var secondLevelXMLDict, tempSecondLevelXMLDict : Dictionary<String, Dictionary<String, String>>?
+  var tempTopicHolder : Dictionary<String, String>?
+  
+  class var sharedInstance : TMNetworkInterface {
+    struct Static {
+      static let _instance : TMNetworkInterface = TMNetworkInterface()
     }
+    return Static._instance
+  }
+  
+  func fetchFirstLevelXMLBatch (completionHandler: (TMNetwork) -> ()) {
+    let url : NSURL = NSURL(string: kFirstLevelXMLBatch)!
+    let urlRequest = NSURLRequest(URL: url)
     
-    func fetchFirstLevelXMLBatch (completionHandler: (TMNetwork) -> ()) {
-        let url : NSURL = NSURL(string: kFirstLevelXMLBatch)!
+    self.currentSession.dataTaskWithRequest(urlRequest,
+      completionHandler: { (data : NSData!, response : NSURLResponse!, error : NSError!) -> Void in
+        if (error != nil) || (data == nil) {
+          //data error
+          completionHandler(TMNetwork.fetchError)
+          return;
+        }
+        //self.firstLevelXMLData = data
+        var xmlparser = NSXMLParser(data: data)
+        xmlparser.delegate = self
+        if xmlparser.parse() == false {
+          self.firstLevelXMLDict = nil
+          completionHandler(TMNetwork.processError)
+          return
+        }
+        self.firstLevelXMLDict = self.tempFirstLevelXMLDict
+        completionHandler(TMNetwork.fetchSuccessful)
+    }).resume()
+  }
+  
+  func fetchSecondLevelXMLTopic(completionHandler: (TMNetwork) -> ()) {
+    if let flxd = firstLevelXMLDict {
+      for (key,url) in flxd {
         let urlRequest = NSURLRequest(URL: url)
-        
-        self.currentSession.dataTaskWithRequest(urlRequest,
-            completionHandler: { (data : NSData!, response : NSURLResponse!, error : NSError!) -> Void in
-                if (error != nil) || (data == nil) {
-                    //data error
-                    completionHandler(TMNetwork.fetchError)
-                    return;
-                }
-                self.firstLevelXMLData = data;
-                var xmlparser = NSXMLParser(data: data);
-                xmlparser.delegate = self
-                if xmlparser.parse() == false {
-                    self.firstLevelXMLDict = nil
-                    completionHandler(TMNetwork.processError)
-                    return
-                }
-                self.firstLevelXMLDict = self.tempFirstLevelXMLDict
-                completionHandler(TMNetwork.fetchSuccessful)
-        }).resume()
-    }
-    
-    func fetchSecondLevelXML(completionHandler: (TMNetwork) -> ()) {
-        //TODO: Think of name that is not 'batch' to call the elements of each file of topics
-        if (firstLevelXMLDict != nil) {}
-        else {
+        self.currentSession.dataTaskWithRequest(urlRequest, completionHandler: { (data : NSData!, response : NSURLResponse!, error : NSError!) -> Void in
+          if (error != nil) || (data == nil) {
             completionHandler(TMNetwork.fetchError)
-        }
-        //TODO: for each XML batch, make special dictionary for the topics XML.  Remember the new xml format!
+            return
+          }
+          var xmlParser = NSXMLParser(data: data)
+          xmlParser.delegate = self
+          if xmlParser.parse() == false {
+            self.secondLevelXMLDict?.removeValueForKey(key)
+            completionHandler(TMNetwork.processError)
+            return;
+          }
+          self.secondLevelXMLDict?.updateValue(tempTopicHolder, forKey: key)
+          completionHandler(TMNetwork.fetchSuccessful)
+        }).resume()
+      }
+      completionHandler(TMNetwork.fetchSuccessful)
+    } else {
+      completionHandler(TMNetwork.fetchUnknown)
     }
-    
-    //TODO:  After your fetchs you should be able to give only the subset the other classes want, rather then the whol thing.  This class should do the hard work of making all of the data seem like one though.
-    
-    //MARK: - NSXMLparserDelegate
-    func parserDidStartDocument(parser: NSXMLParser!) {
-        self.tempFirstLevelXMLDict = Dictionary<String, NSURL>()
+  }
+  
+  //TODO:  After your fetchs you should be able to give only the subset the other classes want, rather then the whol thing.  This class should do the hard work of making all of the data seem like one though.
+  
+  //MARK: - NSXMLparserDelegate
+  func parserDidStartDocument(parser: NSXMLParser!) {
+    self.tempFirstLevelXMLDict = Dictionary<String, NSURL>()
+  }
+  func parser(parser: NSXMLParser!, didStartElement elementName: String!, namespaceURI: String!, qualifiedName qName: String!, attributes attributeDict: [NSObject : AnyObject]!) {
+    if elementName == "batch" {
+      var date : String = attributeDict["date"] as String
+      var url : NSURL = NSURL(string: attributeDict["url"] as String)!
+      self.tempFirstLevelXMLDict?.updateValue(url, forKey: date)
+    } else if elementName == "topic" {
+      tempTopicHolder?.removeAll(keepCapacity: true)
+      if let category = attributeDict["category"] as? String {
+        tempTopicHolder?.updateValue(category, forKey: "category")
+      }
+      if let intro = attributeDict["intro"] as? String {
+        tempTopicHolder?.updateValue(intro, forKey: "intro")
+      }
+      if let min = attributeDict["min"] as? String {
+        tempTopicHolder?.updateValue(min, forKey: "min")
+      }
+      if let max = attributeDict["max"] as? String {
+        tempTopicHolder?.updateValue(max, forKey: "max")
+      }
+      if let source = attributeDict["source"] as? String {
+        tempTopicHolder?.updateValue(source, forKey: "source")
+      }
     }
-    func parser(parser: NSXMLParser!, didStartElement elementName: String!, namespaceURI: String!, qualifiedName qName: String!, attributes attributeDict: [NSObject : AnyObject]!) {
-        if elementName == "batch" {
-            var date : String = attributeDict["date"] as String
-            var url : NSURL = NSURL(string: attributeDict["url"] as String)!
-            self.tempFirstLevelXMLDict?.updateValue(url, forKey: date)
-        }
-    }
-    
+  }
+  func parser(parser: NSXMLParser!, foundCharacters string: String!) {
+    tempTopicHolder?.updateValue(string, forKey: "question")
+  }
+  func parser(parser: NSXMLParser!, didEndElement elementName: String!, namespaceURI: String!, qualifiedName qName: String!) {
+    tempSecondLevelXMLDict
+  }
+}
+
+protocol TMNetworkInterfaceDelegate {
+  
 }
--- TMTopicTests/TMNetworkInterfaceTest.swift
+++ TMTopicTests/TMNetworkInterfaceTest.swift
@@ -14,218 +14,219 @@ let kDummyFirstLevelXMLPath = "DummyFirstLevelXML.xml"
 
 //MARK: test fetching
 class TMNetworkInterfaceDataRequestTest: XCTestCase {
-    let bundle : NSBundle = NSBundle(forClass: TMNetworkInterfaceDataRequestTest.self)
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+  let bundle : NSBundle = NSBundle(forClass: TMNetworkInterfaceDataRequestTest.self)
+  
+  override func setUp() {
+    super.setUp()
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+  }
+  
+  override func tearDown() {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    super.tearDown()
+  }
+  
+  func testNetworkRequestFailed() {
+    //set up Mocks
+    class TestNSURLSession : NSURLSession {
+      // To mock the call for data
+      private override func dataTaskWithRequest(request: NSURLRequest, completionHandler: ((NSData!, NSURLResponse!, NSError!) -> Void)?) -> NSURLSessionDataTask {
+        let dataMock : NSData! = NSData()
+        let urlResponseMock : NSURLResponse! = NSURLResponse()
+        let errorMock : NSError! = NSError()
+        completionHandler!((dataMock, urlResponseMock, errorMock))
+        //fake delay
+        println(String(format:"Begin 'network call'"))
+        sleep(kFakeNetworkDelaySeconds) //sleep 5 seconds
+        println(String(format:"End 'network call'"))
+        return NSURLSessionDataTask()
+      }
     }
     
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-
-    func testNetworkRequestFailed() {
-        //set up Mocks
-        class TestNSURLSession : NSURLSession {
-            // To mock the call for data
-            private override func dataTaskWithRequest(request: NSURLRequest, completionHandler: ((NSData!, NSURLResponse!, NSError!) -> Void)?) -> NSURLSessionDataTask {
-                let dataMock : NSData! = NSData()
-                let urlResponseMock : NSURLResponse! = NSURLResponse()
-                let errorMock : NSError! = NSError()
-                completionHandler!((dataMock, urlResponseMock, errorMock))
-                //fake delay
-                println(String(format:"Begin 'network call'"))
-                sleep(kFakeNetworkDelaySeconds) //sleep 5 seconds
-                println(String(format:"End 'network call'"))
-                return NSURLSessionDataTask()
-            }
+    // plug in
+    let fetchSomethingExpectation : XCTestExpectation = expectationWithDescription("fetching something")
+    var fetchStatus : TMNetwork = .fetchUnknown
+    
+    let networkInterface : TMNetworkInterface = TMNetworkInterface()
+    networkInterface.currentSession = TestNSURLSession()
+    networkInterface.fetchFirstLevelXMLBatch({ (fetchResponse : TMNetwork) in
+      fetchStatus = fetchResponse
+      fetchSomethingExpectation.fulfill()
+    })
+    
+    //Test
+    waitForExpectationsWithTimeout(NSTimeInterval(kFakeNetworkDelaySeconds),
+      handler: { (error : NSError!) -> Void in
+        switch fetchStatus {
+        case .fetchError:
+          XCTAssert(true)
+        default:
+          XCTFail()
         }
-        
-        // plug in
-        let fetchSomethingExpectation : XCTestExpectation = expectationWithDescription("fetching something")
-        var fetchStatus : TMNetwork = .fetchUnknown
-        
-        let networkInterface : TMNetworkInterface = TMNetworkInterface()
-        networkInterface.currentSession = TestNSURLSession()
-        networkInterface.fetchFirstLevelXMLBatch({ (fetchResponse : TMNetwork) in
-            fetchStatus = fetchResponse
-            fetchSomethingExpectation.fulfill()
-        })
-        
-        //Test
-        waitForExpectationsWithTimeout(NSTimeInterval(kFakeNetworkDelaySeconds),
-            handler: { (error : NSError!) -> Void in
-                switch fetchStatus {
-                case .fetchError:
-                    XCTAssert(true)
-                default:
-                    XCTFail()
-                }
-        })
+    })
+  }
+  
+  func testNetworkRequestPassed() {
+    class TestNSURLSessionMock : NSURLSession {
+      private override func dataTaskWithRequest(request: NSURLRequest, completionHandler: ((NSData!, NSURLResponse!, NSError!) -> Void)?) -> NSURLSessionDataTask {
+        var dataMock : NSData! = ("<array></array>") .dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: false);
+        completionHandler!(dataMock, NSURLResponse(), nil)
+        sleep(kFakeNetworkDelaySeconds)
+        return NSURLSessionDataTask()
+      }
     }
     
-    func testNetworkRequestPassed() {
-        class TestNSURLSessionMock : NSURLSession {
-            private override func dataTaskWithRequest(request: NSURLRequest, completionHandler: ((NSData!, NSURLResponse!, NSError!) -> Void)?) -> NSURLSessionDataTask {
-                var dataMock : NSData! = ("<array></array>") .dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: false);
-             completionHandler!(dataMock, NSURLResponse(), nil)
-                sleep(kFakeNetworkDelaySeconds)
-                return NSURLSessionDataTask()
-            }
+    let fetchSomethingExpectation = expectationWithDescription("fetching something")
+    var fetchStatus : TMNetwork = .fetchUnknown
+    
+    let networkInterface = TMNetworkInterface()
+    networkInterface.currentSession = TestNSURLSessionMock()
+    networkInterface.fetchFirstLevelXMLBatch({ (fetchResponse : TMNetwork) -> () in
+      fetchStatus = fetchResponse
+      fetchSomethingExpectation.fulfill()
+    })
+    
+    waitForExpectationsWithTimeout(NSTimeInterval(kFakeNetworkDelaySeconds),
+      handler: { (error : NSError!) -> Void in
+        switch fetchStatus {
+        case .fetchSuccessful:
+          XCTAssert(true)
+        default:
+          XCTFail()
         }
-        
-        let fetchSomethingExpectation = expectationWithDescription("fetching something")
-        var fetchStatus : TMNetwork = .fetchUnknown
-        
-        let networkInterface = TMNetworkInterface()
-        networkInterface.currentSession = TestNSURLSessionMock()
-        networkInterface.fetchFirstLevelXMLBatch({ (fetchResponse : TMNetwork) -> () in
-            fetchStatus = fetchResponse
-            fetchSomethingExpectation.fulfill()
-        })
-        
-        waitForExpectationsWithTimeout(NSTimeInterval(kFakeNetworkDelaySeconds),
-            handler: { (error : NSError!) -> Void in
-                switch fetchStatus {
-                case .fetchSuccessful:
-                    XCTAssert(true)
-                default:
-                    XCTFail()
-                }
-        })
+    })
+  }
+  
+  func testReceivedFirstLevelXML() {
+    //first level xml only contains the post date and
+    var bundle = self.bundle
+    class TestNSURLSessionMock : NSURLSession {
+      private override func dataTaskWithRequest(request: NSURLRequest, completionHandler: ((NSData!, NSURLResponse!, NSError!) -> Void)?) -> NSURLSessionDataTask {
+        var dataMock : NSData! = NSData.dataFrom(
+          contentsOfFile: kDummyFirstLevelXMLPath,
+          encoding: NSUTF8StringEncoding,
+          bundle: TMNetworkInterfaceDataRequestTest().bundle,
+          error: nil)
+        sleep(kFakeNetworkDelaySeconds)
+        completionHandler!(dataMock, NSURLResponse(), nil)
+        return NSURLSessionDataTask()
+      }
     }
-
-    func testReceivedFirstLevelXML() {
-        //first level xml only contains the post date and
-        var bundle = self.bundle
-        class TestNSURLSessionMock : NSURLSession {
-            private override func dataTaskWithRequest(request: NSURLRequest, completionHandler: ((NSData!, NSURLResponse!, NSError!) -> Void)?) -> NSURLSessionDataTask {
-                var dataMock : NSData! = NSData.dataFrom(
-                    contentsOfFile: kDummyFirstLevelXMLPath,
-                    encoding: NSUTF8StringEncoding,
-                    bundle: TMNetworkInterfaceDataRequestTest().bundle,
-                    error: nil)
-                sleep(kFakeNetworkDelaySeconds)
-                completionHandler!(dataMock, NSURLResponse(), nil)
-                return NSURLSessionDataTask()
-            }
+    let fetchSomethingExpectation = expectationWithDescription("fetching dummy data from xml file")
+    var fetchStatus : TMNetwork = .fetchUnknown
+    
+    var networkInterface = TMNetworkInterface()
+    networkInterface.currentSession = TestNSURLSessionMock()
+    networkInterface.fetchFirstLevelXMLBatch { (fetchResponse : TMNetwork) -> () in
+      fetchStatus = fetchResponse
+      fetchSomethingExpectation.fulfill()
+    }
+    
+    waitForExpectationsWithTimeout(NSTimeInterval(kFakeNetworkDelaySeconds), handler: { (error : NSError!) -> Void in
+      switch fetchStatus {
+      case .fetchSuccessful:
+        XCTAssert(true)
+        //var data1 : NSData! = networkInterface.firstLevelXMLData
+        var data1 : NSData! = NSKeyedArchiver.archivedDataWithRootObject(networkInterface.firstLevelXMLDict!)
+        var data2 : NSData! = NSData.dataFrom(contentsOfFile: kDummyFirstLevelXMLPath, encoding: NSUTF8StringEncoding, bundle: self.bundle, error: nil)
+        XCTAssertEqual(data1, data2)
+        let expected = [
+          "2014-11-15" : NSURL(string:"youare.com/a/wonderful/programmer"),
+          "2014-10-1" : NSURL(string:"www.cool.com/i/love/you"),
+          "2014-09-1" : NSURL(string:"http://www.awesome.com"),
+          "2014-08-1" : NSURL(string:"http://www.coolurl.com")
+        ]
+        let actual = networkInterface.firstLevelXMLDict!
+        
+        for key in expected.keys {
+          if let url : NSURL = actual[key] {
+            var actualURL : NSURL! = actual[key]
+            var expectedURL : NSURL! = expected[key]!
+            XCTAssertEqual(actualURL, expectedURL)
+            continue
+          }
+          XCTFail()
         }
-        let fetchSomethingExpectation = expectationWithDescription("fetching dummy data from xml file")
-        var fetchStatus : TMNetwork = .fetchUnknown
-        
-        var networkInterface = TMNetworkInterface()
-        networkInterface.currentSession = TestNSURLSessionMock()
-        networkInterface.fetchFirstLevelXMLBatch { (fetchResponse : TMNetwork) -> () in
-            fetchStatus = fetchResponse
-            fetchSomethingExpectation.fulfill()
-        }
-        
-        waitForExpectationsWithTimeout(NSTimeInterval(kFakeNetworkDelaySeconds), handler: { (error : NSError!) -> Void in
-            switch fetchStatus {
-            case .fetchSuccessful:
-                XCTAssert(true)
-                var data1 : NSData! = networkInterface.firstLevelXMLData
-                var data2 : NSData! = NSData.dataFrom(contentsOfFile: kDummyFirstLevelXMLPath, encoding: NSUTF8StringEncoding, bundle: self.bundle, error: nil)
-                XCTAssertEqual(data1, data2)
-                let expected = [
-                    "2014-11-15" : NSURL(string:"youare.com/a/wonderful/programmer"),
-                    "2014-10-1" : NSURL(string:"www.cool.com/i/love/you"),
-                    "2014-09-1" : NSURL(string:"http://www.awesome.com"),
-                    "2014-08-1" : NSURL(string:"http://www.coolurl.com")
-                ]
-                let actual = networkInterface.firstLevelXMLDict!
-                
-                for key in expected.keys {
-                    if let url : NSURL = actual[key] {
-                        var actualURL : NSURL! = actual[key]
-                        var expectedURL : NSURL! = expected[key]!
-                        XCTAssertEqual(actualURL, expectedURL)
-                        continue
-                    }
-                    XCTFail()
-                }
-            default:
-                XCTFail()
-            }
-        })
-    }
-
-    func testReceivedSecondLevelXMLFromEachFirstLevelXML() {
+      default:
+        XCTFail()
+      }
+    })
+  }
+  
+  func testReceivedSecondLevelXMLFromEachFirstLevelXML() {
     
-    }
+  }
+  
+  func testReceivedSecondLevelXMLWhenOnlyOneFirstLevelXMLIsNew() {
     
-    func testReceivedSecondLevelXMLWhenOnlyOneFirstLevelXMLIsNew() {
-        
-    }
+  }
+  
+  func testReceivedNoSecondLevelXMLBecauseDeviceHasAllOfThemAlready() {
     
-    func testReceivedNoSecondLevelXMLBecauseDeviceHasAllOfThemAlready() {
-        
-    }
-//    //move this function to a data extension
-//    func xmlStringContentsToData(filename: String) -> NSData? {
-//        var data : NSData? = nil;
-//        let bundle = NSBundle(forClass: TMNetworkInterfaceDataRequestTest.self)
-//        let path = bundle.pathForResource(filename, ofType: nil)
-//        var err : NSErrorPointer = nil;
-//        if let content = String(
-//            contentsOfFile: path!,
-//            encoding: NSUTF8StringEncoding,
-//            error: err) {
-//                if let _data = content.dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: false) {
-//                    data = _data
-//                    return data
-//                }
-//        }
-//        XCTFail("Could not read file")
-//        return data
-//    }
+  }
+  //    //move this function to a data extension
+  //    func xmlStringContentsToData(filename: String) -> NSData? {
+  //        var data : NSData? = nil;
+  //        let bundle = NSBundle(forClass: TMNetworkInterfaceDataRequestTest.self)
+  //        let path = bundle.pathForResource(filename, ofType: nil)
+  //        var err : NSErrorPointer = nil;
+  //        if let content = String(
+  //            contentsOfFile: path!,
+  //            encoding: NSUTF8StringEncoding,
+  //            error: err) {
+  //                if let _data = content.dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: false) {
+  //                    data = _data
+  //                    return data
+  //                }
+  //        }
+  //        XCTFail("Could not read file")
+  //        return data
+  //    }
 }
 
 //MARK: notify all listeners of data
 class TMNetworkInterfaceNotificationTest: XCTestCase {
+  
+  override func setUp() {
+    super.setUp()
     
-    override func setUp() {
-        super.setUp()
+  }
+  override func tearDown() {
     
-    }
-    override func tearDown() {
+    super.tearDown()
+  }
+  
+  func testThatDataWasSentToDelegatePassing() {
     
-        super.tearDown()
-    }
+  }
+  
+  func testThatDataWasSentToDelegateFailing() {
     
-    func testThatDataWasSentToDelegatePassing() {
-        
-    }
+  }
+  
+  func testThatDataWasPostedToNotificationCenterPassing() {
     
-    func testThatDataWasSentToDelegateFailing() {
-        
-    }
+  }
+  
+  func testThatDataWasPostedToNotificationCenterFailing() {
     
-    func testThatDataWasPostedToNotificationCenterPassing() {
-        
-    }
-    
-    func testThatDataWasPostedToNotificationCenterFailing() {
-        
-    }
+  }
 }
 
 //TODO: move this in to its own file
 extension NSData {
-    class func dataFrom(contentsOfFile filename: String!, encoding: NSStringEncoding, bundle: NSBundle!, error: NSErrorPointer) -> NSData? {
-        let path = bundle.pathForResource(filename, ofType: nil)
-        if let content = String(
-            contentsOfFile: path!,
-            encoding: encoding,
-            error: error)
-        {
-            if let _data = content.dataUsingEncoding(encoding, allowLossyConversion: false)
-            {
-                return _data
-            }
-        }
-        return nil
+  class func dataFrom(contentsOfFile filename: String!, encoding: NSStringEncoding, bundle: NSBundle!, error: NSErrorPointer) -> NSData? {
+    let path = bundle.pathForResource(filename, ofType: nil)
+    if let content = String(
+      contentsOfFile: path!,
+      encoding: encoding,
+      error: error)
+    {
+      if let _data = content.dataUsingEncoding(encoding, allowLossyConversion: false)
+      {
+        return _data
+      }
     }
+    return nil
+  }
 }
